17427fe2ac3b1ba154e743a612c4f70751b683c0
Divyansh Singhvi <divyanshsinghvi@gmail.com>
2018-06-30 17:00:01 +0530

    pluginlist.js updated

--- js/pluginlist.js
+++ js/pluginlist.js
@@ -1,8 +1,11 @@
 var serverurl="http://localhost:8000"
-var pluginlist = "";
+/*var pluginlist = "";
     fs.readdirSync(pluginFolder).forEach(file => {
                 pluginlist =  pluginlist + '<a id='+String(file)+' href="config">'+String(file)+'</a>';
                 //console.log(file);
         });
-document.getElementById("#kibanadiv").innerHTML = pluginlist
+*/
+var pluginlist ='<a id=youtube href="config">youtube</a> '
+$(document).on("click","a",function(e){ e.preventDefault();document.getElementById("kibanadiv").appendChild("http://localhost:8000/plugins/"+$(this).attr("id")+".js")});
+document.getElementById("kibanadiv").innerHTML = pluginlist
 
