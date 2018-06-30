0a0a0beb55e33d022ce29233959b0f35e8a70b46
Rajat Jain <1997.rajatjain@gmail.com>
2018-06-30 19:17:10 +0530

    Removed somethings

--- kibana-html-plugin/public/htmlController.js
+++ kibana-html-plugin/public/htmlController.js
@@ -8,15 +8,22 @@ module.controller('KbnHtmlEditController', ['$scope', function($scope) {
 }]);
 
 module.controller('KbnHtmlVisController', function ($scope, $sce) {
-       $scope.html = $sce.trustAsHtml('<!DOCTYPE html><html><body><div id="kibanadiv"> </div> <script src="http://localhost:8000/js/pluginlist.js" id="kibanascript"></script> </body></html>');
+       //$scope.html = $sce.trustAsHtml('<!DOCTYPE html><html><body><div id="kibanadiv"> </div> <script src="http://localhost:8000/js/pluginlist.js" id="kibanascript"></script> </body></html>');
        
        $scope.$watch('renderComplete', function () {
   
   var html = $scope.vis.params.html;
-    if ($scope.updateStatus.params ){
+  console.log($scope.vis);
+ 
+    if (html){
     	//$scope.html = $sce.trustAsHtml('<!DOCTYPE html><html><body><div id="thatswhatshesaid"> </div><script>var HttpClient = function() {this.get = function(aUrl, aCallback) {var anHttpRequest = new XMLHttpRequest();anHttpRequest.onreadystatechange = function() { if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)aCallback(anHttpRequest.responseText);};anHttpRequest.open( "GET", aUrl, true );anHttpRequest.send( null );}};var client = new HttpClient();client.get(\'http://localhost:8000/pluginList\',function(response){console.log(response);document.getElementById("thatswhatshesaid").innerHTML = response;});</script></body></html>');
     //  $scope.html = $sce.trustAsHtml('<!DOCTYPE html><html><body><div id="thatswhatshesaid"> </div><script></script></body></html>');
-    $scope.html = $sce.trustAsHtml('<!DOCTYPE html><html><body> </body></html>');
+    console.log(html);
+    var a='<iframe src=';
+    var b=' width="100%" height="100%"></iframe>';
+    var c=html.match(/<p.*id=\"redirectURL\"[!>]*>(.*)<\/p>/gm);
+    console.log(c);
+    $scope.html = $sce.trustAsHtml('<!DOCTYPE html><html><body>'+a+c+b+'</body></html>');
        
      }
 
