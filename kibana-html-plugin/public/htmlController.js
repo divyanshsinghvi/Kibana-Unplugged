c74ac66925db5ca740b00c8e87b82a3e7d534125
Rajat Jain <1997.rajatjain@gmail.com>
2018-06-30 19:06:42 +0530

    LHS

--- kibana-html-plugin/public/htmlController.js
+++ kibana-html-plugin/public/htmlController.js
@@ -15,7 +15,9 @@ module.controller('KbnHtmlVisController', function ($scope, $sce) {
   var html = $scope.vis.params.html;
     if ($scope.updateStatus.params ){
     	//$scope.html = $sce.trustAsHtml('<!DOCTYPE html><html><body><div id="thatswhatshesaid"> </div><script>var HttpClient = function() {this.get = function(aUrl, aCallback) {var anHttpRequest = new XMLHttpRequest();anHttpRequest.onreadystatechange = function() { if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)aCallback(anHttpRequest.responseText);};anHttpRequest.open( "GET", aUrl, true );anHttpRequest.send( null );}};var client = new HttpClient();client.get(\'http://localhost:8000/pluginList\',function(response){console.log(response);document.getElementById("thatswhatshesaid").innerHTML = response;});</script></body></html>');
-     $scope.html = $sce.trustAsHtml('<!DOCTYPE html><html><body><div id="thatswhatshesaid"> </div><script></script></body></html>');
+    //  $scope.html = $sce.trustAsHtml('<!DOCTYPE html><html><body><div id="thatswhatshesaid"> </div><script></script></body></html>');
+    $scope.html = $sce.trustAsHtml('<!DOCTYPE html><html><body> </body></html>');
+       
      }
 
     $scope.renderComplete();
